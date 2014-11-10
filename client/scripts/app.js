d0beec730d8902c55c2708a7a211ad44df89c8d2
christophertrev <christophertrev@gmail.com>
2014-11-10 15:43:55 -0800

    add unique message tracking, but broke server interaction

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -3,35 +3,34 @@
 
 $(document).ready(function(){
 
+var messagesPosted = {};
+
 var appendMessages = function(data){
   console.log(data);
   _.each(data.results, function(el){
-    var tweet = $('<div>').text(el.text);
-    $('.chatWindow').append(tweet);
+    if (!messagesPosted[el.objectId]){
+      var tweet = $('<div>').text(el.text);
+      $('.chatWindow').append(tweet);
+      messagesPosted[el.objectId]=true;
+    }
   })
 }
-//post messages
-  //clear any existing messages in window
-  //$('.chatWindow').text('')
-  //get most recent 10 from server
-  //save them as var
+
+// var fetchMessages = function(){
+  console.log('aquiring New BroTexts');
   $.ajax({
   // always use this url
     url: 'https://api.parse.com/1/classes/chatterbox',
     type: 'GET',
-    //data:{'username':"jillian"},
+    //data:{'results.username':'jillian'},
     success: appendMessages,
     error: function (data) {
     // see: https://developer.mozilla.org/en-US/docs/Web/API/console.error
       console.error('chatterbox: Failed to get message');
     }
   });
-  //create new HTML node for each of those messages
-  //escaping the text
-  //append each node to the chat window
+// };
 
-
-
-
+// setInterval(fetchMessages,1000)
 
 })
