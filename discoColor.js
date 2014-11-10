2433aa710513bb6d35d75b275b23e9679c891c4f
christophertrev <christophertrev@gmail.com>
2014-11-10 11:50:19 -0800

    add discoColor snippet

---
+++ discoColor.js
@@ -0,0 +1,13 @@
+
+var colorCycle = function(){
+  this.style.backgroundColor = '#'+Math.floor(Math.random()*16777215).toString(16);
+};
+
+var discoTime = function(){
+  var elements = document.getElementsByTagName("*");
+  for (var i = 0; i < elements.length; i++) {
+    setInterval(colorCycle.bind(elements[i]),50+i);
+  }
+};
+
+
