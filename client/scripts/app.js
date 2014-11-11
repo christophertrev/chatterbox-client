f89941cf4272151a1f38c81278a59a4111dc60d6
christophertrev <christophertrev@gmail.com>
2014-11-10 16:54:36 -0800

    Implement Post Message behavior.

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,37 +1,66 @@
 // YOUR CODE HERE:
 
+var username = window.location.search.split('=')[1];
 
 $(document).ready(function(){
 
+
+//Global Variables
 var messagesPosted = {};
+//var lastFetchTime = "2012-11-11T00:19:14.089Z";
 
 var appendMessages = function(data){
-  console.log(data);
-  _.each(data.results, function(el){
+  console.log(data)
+  _.each(data.results.reverse(), function(el){
     if (!messagesPosted[el.objectId]){
-    //console.log('posting New Message')
-       var tweet = $('<div>').text(el.text);
-       $('.chatWindow').append(tweet);
+       var tweet = $('<div>').text(el.createdAt + " " + el.username + ": " + el.text);
+       $('.chatWindow').prepend(tweet);
       messagesPosted[el.objectId]=true;
     }
   })
 }
 
  var fetchMessages = function(){
-  console.log('aquiring New BroTexts');
+  console.log('fetching Massages')
   $.ajax({
   // always use this url
     url: 'https://api.parse.com/1/classes/chatterbox',
     type: 'GET',
-    //data:{'results.username':'jillian'},
+    //data:'where={"createdAt":{"$gt":'+lastFetchTime+'}}',
+    data:'order=-createdAt',
     success: appendMessages,
     error: function (data) {
     // see: https://developer.mozilla.org/en-US/docs/Web/API/console.error
       console.error('chatterbox: Failed to get message');
     }
   });
+  //lastFetchTime= (new Date()).toJSON();
  };
-//fetchMessages();
-setInterval(fetchMessages,1000)
+
+fetchMessages();
+setInterval(fetchMessages,5000)
 
 })
+
+
+var postMessage = function(string) {
+  var message = {
+  'username': username,
+  'text': string,
+  'roomname': 'bouncyCastle 2.0'
+  };
+
+  $.ajax({
+    url:'https://api.parse.com/1/classes/chatterbox',
+    type: 'POST',
+    data: JSON.stringify(message),
+    contentType: 'application/json',
+    success: function(){
+      console.log('chatterbox: Message sent');
+    },
+    error: function (data) {
+    // see: https://developer.mozilla.org/en-US/docs/Web/API/console.error
+      console.error('chatterbox: Failed to post message');
+    }
+  });
+};
