661e02a7c2d1b6ebda1b0bcc57aebadce2017f9d
christophertrev <christophertrev@gmail.com>
2014-11-11 12:14:53 -0800

    Add click functionality to username to friend them.
    Change time to more readable format.
    remove comments to improve readablity

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -7,7 +7,14 @@ $(document).ready(function(){
   var messagesPosted = {};
   var userName = window.location.search.split('=')[1];
   var roomFilter = null;
-  //var lastFetchTime = "2012-11-11T00:19:14.089Z";
+  var friends = {};
+
+  var scrubber = function(string){
+    if(string){
+      return string.replace(/[^a-z 0-9\?,.!@#$%^&*]/gi,'__');
+    }
+    return 'undefined'
+  }
 
   var postMessage = function(string) {
     var message = {
@@ -25,7 +32,6 @@ $(document).ready(function(){
         console.log('chatterbox: Message sent');
       },
       error: function (data) {
-      // see: https://developer.mozilla.org/en-US/docs/Web/API/console.error
         console.error('chatterbox: Failed to post message');
       }
     });
@@ -50,21 +56,16 @@ $(document).ready(function(){
 
     console.log('fetching Massages')
     $.ajax({
-    // always use this url
       url: 'https://api.parse.com/1/classes/chatterbox',
       type: 'GET',
       //data:'where={"createdAt":{"$gt":'+lastFetchTime+'}}',
   //    data:'order=-createdAt&where={"username":"brian"}',
-       // data:JSON.stringify({ username:"brian" }),
       data:dataString,
-    // data:'order=-createAt',
       success: successCallback,
       error: function (data) {
-      // see: https://developer.mozilla.org/en-US/docs/Web/API/console.error
         console.error('chatterbox: Failed to get message');
       }
     });
-    //lastFetchTime= (new Date()).toJSON();
    };
 
   var fetchMessagesByRoom = function(roomName,successCallback){
@@ -79,13 +80,37 @@ $(document).ready(function(){
     console.log(data)
     _.each(data.results.reverse(), function(el){
       if (!messagesPosted[el.objectId] && el.username!== 'MOOSE' && el.roomname!=='4chan'){
-        var tweet = $('<div>').text(el.createdAt + " " + el.username + ": " + el.text + "   ---" + el.roomname);
-        tweet.addClass('message')
-        $('.chatWindow').prepend(tweet);
+        var $message = getMessageNode(el);
+        $('.chatWindow').prepend($message);
         messagesPosted[el.objectId]=true;
       }
     })
   }
+  var getMessageNode = function(msgObject){
+    var $message = $('<div>').addClass('message');
+    var $userName = $('<span>').text(scrubber(msgObject.username))
+            .addClass('userName').on('click',function(){
+              friends[scrubber(msgObject.username)]=true;
+              console.log(friends);
+              $('.userName:contains('+$userName.text()+')').addClass('friend')
+            }); // this could be refactored out for clarity?
+    var $timeStamp = $('<span>').text((new Date(msgObject.createdAt).toLocaleTimeString())) //this could also be refatored out for clarity
+            .addClass('timeStamp');
+    var $roomName = $('<span>').text(scrubber(msgObject.roomname))
+            .addClass('roomName');
+    if(friends[$userName.text()]){
+      $userName.addClass('friend')
+    }
+
+    $message.append($timeStamp)
+        .append(':  ')
+        .append($userName)
+        .append(' ')
+        .append(scrubber(msgObject.text))
+        .append('              --')
+        .append($roomName)
+    return $message
+  };
 
   var addRoomButtons = function(){
     var buttons = {}
@@ -93,7 +118,7 @@ $(document).ready(function(){
       _.each(data.results,function(el){
         if(!buttons[el.roomname] && el.roomname){
           var $button = $('<button>')
-            .val(el.roomname)
+            .val(el.roomname)///// this might enable xss attacks if we are not careful
             .text(el.roomname)
             .addClass('filterButton')
             .on('click',filterByRoom);
@@ -111,7 +136,6 @@ $(document).ready(function(){
     roomFilter = roomName;
     fetchMessagesByRoom(roomName);
     console.log(roomName===null);
-    // intervalID = setInterval(fetchMessages.bind(null,null,roomname,appendMessages),5000)
   };
 
 
