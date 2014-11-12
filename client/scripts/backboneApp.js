c660f10f52306ddfab867968135cbf6b99931664
Christopher Treviño <christophertrev@gmail.com>
2014-11-12 09:01:02 -0800

    Partial backbone implimenation.

--- client/scripts/backboneApp.js
+++ client/scripts/backboneApp.js
@@ -7,12 +7,42 @@ var Messages = Backbone.Collection.extend( {
   model: Message,
   url: 'https://api.parse.com/1/classes/chatterbox',
   parse: function(response,options){
-    return response.results};
+    return response.results},
   fetchMessages: function(){
     this.fetch({data:{ order: '-createdAt' }});
   }
-} );
+});
 
+var MessagesView = Backbone.View.extend({
+
+  initialize: function(){
+    this.collection.on('sync',this.render,this)
+  },
+
+  render: function(){
+    this.collection.forEach(this.renderMessages,this);
+    return this.$el;
+  },
+
+
+  renderMessages: function(message){
+    var message = new MessageView({model:message})
+    this.$el.prepend(message.render());
+  },
+
+})
+
+var MessageView = Backbone.View.extend({
+  template: _.template('<div class="message"><span class="timeStamp">9:55:02 PM</span>: \
+    <span class="username"><%= username %></span> s --     \
+    <span class="roomName"><%= roomname %></span></div>'),
+  render: function(){
+    debugger;
+    this.$el.html(this.template(this.model.attributes))
+    return this.$el
+  }
+
+});
 
 //render messages from server
 
